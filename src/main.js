db44a5464d4a83d4c0c478a39888f7a5ce37bb09
sckickar <r0guehaxx@gmail.com>
2026-09-25 09:47:38 +0800

    formatting changes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,18 @@
-import { VW, VH, MAX_ZOOM } from './core/constants.js';
-import { Boot } from './scenes/Boot.js';
-import { Title } from './scenes/Title.js';
-import { Game } from './scenes/Game.js';
-import { UI } from './scenes/UI.js';
+import { VW, VH, MAX_ZOOM } from "./core/constants.js";
+import { Boot } from "./scenes/Boot.js";
+import { Title } from "./scenes/Title.js";
+import { Game } from "./scenes/Game.js";
+import { UI } from "./scenes/UI.js";
 
 const game = new Phaser.Game({
   type: Phaser.WEBGL,
-  parent: 'game',
-  width: VW, height: VH,
-  backgroundColor: '#000000',
-  pixelArt: true, roundPixels: true, antialias: false,
+  parent: "game",
+  width: VW,
+  height: VH,
+  backgroundColor: "#000000",
+  pixelArt: true,
+  roundPixels: true,
+  antialias: false,
   input: { activePointers: 3 },
   scale: { mode: Phaser.Scale.NONE, autoCenter: Phaser.Scale.CENTER_BOTH },
   scene: [Boot, Title, Game, UI],
@@ -20,6 +23,6 @@ function fit() {
   game.scale.setZoom(Math.max(1, Math.min(MAX_ZOOM, s)));
 }
 
-window.addEventListener('resize', fit);
-game.events.once('ready', fit);
+window.addEventListener("resize", fit);
+game.events.once("ready", fit);
 window.game = game;
